f5736735772df8d865c1c490cbff411b4a32fb56
@christinaszele <43682308+cszele@users.noreply.github.com>
2019-08-02 23:04:40 -0700

    Add files via upload

---
+++ swHelper.js
@@ -0,0 +1,59 @@
+let staticCache = 'rest-cache';
+
+let cacheURLs = [
+  './',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './data/restaurants.json',
+  './js/dbhelper.js',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+  './css/grid.css',
+  './swHelper.js'
+];
+
+//install
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCache).then(function(cache) {
+      console.log('Cache Opened');
+      return cache.addAll(cacheURLs);
+    })
+    .catch((err) => {
+      console.log('Cache Error: ' + err);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Finally active. Ready to start serving content!');
+});
+
+// fetch
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+    caches.match(event.request).then(function(response) {
+      console.log(event.request.url);
+      if (response) {
+          return response;
+        }
+      return fetch(event.request);
+    })
+  );
+});
+
+
+// referenced:
+// https://developer.mozilla.org/en-US/docs/Web/API/Cache
+// https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
